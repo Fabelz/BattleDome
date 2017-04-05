6dbaf6782e4a7c90a49fac3f64d255b8e12b4999
fabelz <raposalife@gmail.com>
2017-04-05 21:44:44 +1000

    Initial commit

---
+++ src/BD/Core.java
@@ -0,0 +1,19 @@
+package BD;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * placeholder
+ */
+public class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
