0bc0a4d6cd8de2a94eef3d6d9fee2905773c1bee
fabelz <raposalife@gmail.com>
2017-04-05 22:34:14 +1000

    Second commit.

--- src/BD/Core.java
+++ src/BD/Core.java
@@ -1,5 +1,6 @@
 package BD;
 
+import BD.GameStates.States;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -7,8 +8,36 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Core extends JavaPlugin {
 
+    private static int ticks = 0;
+    private static States states;
+
+    public static States getState() {
+        return states;
+    }
+
+    public static void setState(States states) {
+        Core.states = states;
+    }
+
+    public static int getTicks() {
+        return ticks;
+    }
+
+    public static void setTicks(int ticks) {
+        Core.ticks = ticks;
+    }
+
+    private static Core instance;
+
+    public static Core getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
+        instance = this;
+        ticks = 0;
+        states = States.LOBBY;
 
     }
 
---
+++ src/BD/GameStates/States.java
@@ -0,0 +1,8 @@
+package BD.GameStates;
+
+/**
+ * placeholder text.
+ */
+public enum States {
+    LOBBY, PREGAME, INGAME, ENDED
+}
